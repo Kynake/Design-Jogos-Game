5e2196c791956838251a61f147b6b470e7fbea9e
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-03-26 02:02:48 -0300

    Removed unused Flaming Asteroid variables

--- Assets/Scripts/FlamingAsteroidController.cs
+++ Assets/Scripts/FlamingAsteroidController.cs
@@ -5,15 +5,10 @@ using UnityEngine;
 public class FlamingAsteroidController : AsteroidController
 {
     public GameObject itemPrefab;
-    private bool disabledByLifetime = false;
-
-    private static ObjectPool _itemPool = null;
-
 
     protected override IEnumerator destroyAfter()
     {
         yield return new WaitForSeconds(lifetime);
-        disabledByLifetime = true;
         gameObject.SetActive(false);
     }
 
